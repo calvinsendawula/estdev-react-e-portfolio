8b61cd94498699cd123bce22a2453fdb09d7130c
calvinsendawula <calvin.sendawula@strathmore.edu>
2024-01-29 00:31:25 +0300

    feat: complete services page

---
+++ src/components/skill-cards.js
@@ -0,0 +1,96 @@
+const SkillCards = () => {
+    return ( 
+        <div className="skill-cards">
+            <div class="skill-cards-1">
+                <div>
+                    <ul>
+                        <li><h3>Internet Application Programming</h3></li>
+                        <li>Laravel Framework</li>
+                        <li>PHP, HTML5, CSS</li>
+                        <li>Bootstrap 5</li>
+                        <li>JavaScript</li>
+                        <li>Ajax</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li><h3>Procedural & Object Oriented Programming</h3></li>
+                        <li>Java, JavaFX</li>
+                        <li>Python</li>
+                        <li>C++</li>
+                        <li>PHP</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li><h3>Software Engineering & UI/UX Design</h3></li>
+                        <li>Figma</li>
+                        <li>AdobeXD</li>
+                        <li>Canva</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li><h3>Animation</h3></li>
+                        <li>Blender</li>
+                        <li>Daz3D</li>
+                        <li>ThreeJS</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="skill-cards-2">
+                <div>
+                    <ul>
+                        <li><h3>Artificial Intelligence</h3></li>
+                        <li>AI Agents</li>
+                        <li>Propositional Logic Algorithms</li>
+                        <li>Search Strategies</li>
+                        <li>K-Nearest Neighbors</li>
+                        <li>Chat Bots</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li><h3>Machine Learning</h3></li>
+                        <li>Linear & Logistic Regression</li>
+                        <li>Natural Language Models</li>
+                        <li>Computer Vision</li>
+                        <li>Artificial & Recurrent Neural Networks</li>
+                        <li>Generative Adversarial Networks</li>
+                        <li>Deep Learning</li>
+                        <li>Supervised & Unsupervised Learning</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li><h3>ETL, Data Warehousing & Visualization, & Database MGT</h3></li>
+                        <li>MySQL</li>
+                        <li>SQLite</li>
+                        <li>Tableau</li>
+                        <li>Power BI</li>
+                        <li>Apache Spark</li>
+                        <li>Apache Kafka</li>
+                        <li>Talend</li>
+                        <li>PhpMyAdmin</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul id="last-ul">
+                        <li><h3>Data Structures & Algorithms</h3></li>
+                        <li>Arrays</li>
+                        <li>Linked Lists</li>
+                        <li>Stacks</li>
+                        <li>Queues</li>
+                        <li>Trees</li>
+                        <li>Graphs</li>
+                        <li>Hash Tables</li>
+                        <li>Sorting Algorithms</li>
+                        <li>Searching Algorithms</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default SkillCards;
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,9 +1,13 @@
 import Skillsets from "../components/skillsets"
+import SkillCards from "../components/skill-cards";
+import NavBar from "../components/navbar";
 
 const Services = () => {
     return ( 
         <div className="main-section">
             <Skillsets />
+            <SkillCards />
+            <NavBar />
         </div>
      );
 }
