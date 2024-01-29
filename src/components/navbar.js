174c9903d621c241c459b2c92405d7802f31db4c
calvinsendawula <calvin.sendawula@strathmore.edu>
2024-01-29 22:14:27 +0300

    fix: revert to commit before first deployment

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,12 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import ResumePDF from "../assets/Sendawula Calvin Resume.pdf";
 
 const NavBar = () => {
     return ( 
         <div className="footer-nav-section">
             <div className="footer-nav">
-                {/* <Link to="/">Home</Link>
-                <Link to="/services">Services</Link> */}
-                <a href="">Home</a>
-                <a href="">Services</a>
+                <Link to="/">Home</Link>
+                <Link to="/services">Services</Link>
                 <a href={ ResumePDF } download="Sendawula Calvin Resume">My Resume</a>
             </div>
         </div>
