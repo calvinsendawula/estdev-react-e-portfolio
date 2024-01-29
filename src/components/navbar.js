4479d1f43a0308116046f25a3bfbb149805a4ac8
calvinsendawula <calvin.sendawula@strathmore.edu>
2024-01-29 22:49:56 +0300

    feat: revert and rebuild

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,10 @@
-import { Link } from "react-router-dom";
 import ResumePDF from "../assets/Sendawula Calvin Resume.pdf";
 
 const NavBar = () => {
     return ( 
         <div className="footer-nav-section">
             <div className="footer-nav">
-                <Link to="/">Home</Link>
-                <Link to="/services">Services</Link>
-                <a href={ ResumePDF } download="Sendawula Calvin Resume">My Resume</a>
+                <a href={ ResumePDF } download="Sendawula Calvin Resume">Download My Resume Here</a>
             </div>
         </div>
      );
--- src/components/skillsets.js
+++
@@ -1,9 +0,0 @@
-const Skillsets = () => {
-    return ( 
-        <div className="skillsets">
-            <h2>My Services</h2>
-        </div>
-     );
-}
- 
-export default Skillsets;
