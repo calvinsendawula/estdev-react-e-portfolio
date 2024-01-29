292263fc36584198a6fab3ea8e1bba4c66b37381
calvinsendawula <calvin.sendawula@strathmore.edu>
2024-01-29 21:27:43 +0300

    feat: add components and update navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Services from "./pages/services";
+import MyName from "./components/my-name";
+import Greeting from "./components/greeting";
+import ShortIntro from "./components/short-intro";
+import SkillsSection from "./components/skills-section";
+import RandomJoke from "./components/random-joke";
+import SocialLinks from "./components/social-links";
+import NavBar from "./components/navbar";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={ <Home /> } />
-        <Route path="/services" element={ <Services /> }/>
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route exact path="/" element={ <Home /> } />
+    //     <Route path="/services" element={ <Services /> }/>
+    //   </Routes>
+    // </Router>
+    <div className="main-section">
+      <MyName />
+      <Greeting />
+      <ShortIntro />
+      <SkillsSection />
+      <RandomJoke />
+      <SocialLinks />
+      <NavBar />
+  </div>
   );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import ResumePDF from "../assets/Sendawula Calvin Resume.pdf";
 
 const NavBar = () => {
     return ( 
         <div className="footer-nav-section">
             <div className="footer-nav">
-                <Link to="/">Home</Link>
-                <Link to="/services">Services</Link>
+                {/* <Link to="/">Home</Link>
+                <Link to="/services">Services</Link> */}
+                <a href="">Home</a>
+                <a href="">Services</a>
                 <a href={ ResumePDF } download="Sendawula Calvin Resume">My Resume</a>
             </div>
         </div>
